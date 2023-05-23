743eb694b8e6f25e4694d653dfb40fa2bdf7e213
orxanmuradli <orxanmuradli1907@gmail.com>
2023-05-23 16:08:32 +0400

    csdcscd

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import './App.css'
-import Header from './components/Header/Navbar'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Pages/Home/Home';
-import Popular from './components/Pages/Home/Popular/Popular';
-import Blog from './components/Pages/Blog/Blog';
+import "./App.css";
+import Header from "./components/Header/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Pages/Home/Home";
+import Popular from "./components/Pages/Home/Popular/Popular";
+import Blog from "./components/Pages/Blog/Blog";
 
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 // import Menu from './components/Pages/Menu/Menu';
 // import Menu from './components/Pages/Menu/Menu';
 function App() {
-return <div className='App'>
-  <Router>
-  <Header/>
+  return (
+    <div className="App">
+      <Router>
+        <Header />
 
-  <Routes>
-    <Route path='/' exact Component={Home}/>
-  </Routes>
- <Popular/>
-  
-  <Blog/>
-  <Footer/>
-  </Router>
-</div>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+        </Routes>
+        <Popular />
+
+        <Blog />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
-
-
-
+export default App;
